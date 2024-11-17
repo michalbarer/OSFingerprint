94a0f48a07ebcf95a3998d4c2bd527626c08abbd
michalbarer <michal.barer@honeybook.com>
2024-11-17 13:19:25 +0200

    separate tcp and seq tcp probes

--- probes/tcp.py
+++ probes/tcp.py
@@ -14,24 +14,17 @@ class TCPProbe(Probe):
     def __init__(self, target_ip, open_port, closed_port):
         super().__init__(target_ip)
         self.open_port = open_port
-        self.closed_port = closed_port  # todo - do we really need open & closed ports
-        self.probe_configs = [
-            {"flags": "", "df": True, "window": 128, "port": self.open_port},  # T2 - Null flag
-            {"flags": "SFUP", "df": False, "window": 256, "port": self.open_port},  # T3 - SYN, FIN, URG, PSH
-            {"flags": "A", "df": True, "window": 1024, "port": self.open_port},  # T4 - ACK
-            {"flags": "S", "df": False, "window": 31337, "port": self.closed_port},  # T5 - SYN
-            {"flags": "A", "df": True, "window": 32768, "port": self.closed_port},  # T6 - ACK
-            {"flags": "FPU", "df": False, "window": 65535, "port": self.closed_port}  # T7 - FIN, PSH, URG
-        ]
+        self.closed_port = closed_port
+        self.probe_config = {}
         self.responses = []
 
     def send_probe(self):
-        for config in self.probe_configs:
+        if self.probe_config:
             ip_packet = IP(dst=self.target_ip)
             tcp_packet = TCP(
-                dport=config["port"],
-                flags=config["flags"],
-                window=config["window"],
+                dport=self.probe_config["port"],
+                flags=self.probe_config["flags"],
+                window=self.probe_config["window"],
                 options=[
                     ("WScale", 15),
                     ("NOP", None),
@@ -57,3 +50,45 @@ class TCPProbe(Probe):
                 print(f"  Window Size: {tcp_layer.window}")
             else:
                 print(f"TCP Flag Probe T{i} received no response.")
+
+
+class T2Probe(TCPProbe):
+    """ TCP Flag Probe T2 """
+    def __init__(self, target_ip, open_port):
+        super().__init__(target_ip, open_port, None)
+        self.probe_configs = {"flags": "", "df": True, "window": 128, "port": self.open_port}
+
+
+class T3Probe(TCPProbe):
+    """ TCP Flag Probe T3 """
+    def __init__(self, target_ip, open_port):
+        super().__init__(target_ip, open_port, None)
+        self.probe_configs = {"flags": "SFUP", "df": False, "window": 256, "port": self.open_port}
+
+
+class T4Probe(TCPProbe):
+    """ TCP Flag Probe T4 """
+    def __init__(self, target_ip, open_port):
+        super().__init__(target_ip, open_port, None)
+        self.probe_configs = {"flags": "A", "df": True, "window": 1024, "port": self.open_port}
+
+
+class T5Probe(TCPProbe):
+    """ TCP Flag Probe T5 """
+    def __init__(self, target_ip, closed_port):
+        super().__init__(target_ip, None, closed_port)
+        self.probe_config = {"flags": "S", "df": False, "window": 31337, "port": self.closed_port}
+
+
+class T6Probe(TCPProbe):
+    """ TCP Flag Probe T6 """
+    def __init__(self, target_ip, closed_port):
+        super().__init__(target_ip, None, closed_port)
+        self.probe_config = {"flags": "A", "df": True, "window": 32768, "port": self.closed_port}
+
+
+class T7Probe(TCPProbe):
+    """ TCP Flag Probe T7 """
+    def __init__(self, target_ip, closed_port):
+        super().__init__(target_ip, None, closed_port)
+        self.probe_config = {"flags": "FPU", "df": False, "window": 65535, "port": self.closed_port}
